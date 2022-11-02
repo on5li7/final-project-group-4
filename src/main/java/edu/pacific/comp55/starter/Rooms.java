e374b9dce7d179c10e998720717f76a6a867d644
Clint <c_penafiel@u.pacific.edu>
2022-11-02 14:29:42 -0700

    Commented out the model so there's no error

--- src/main/java/edu/pacific/comp55/starter/Rooms.java
+++ src/main/java/edu/pacific/comp55/starter/Rooms.java
@@ -1,21 +1,12 @@
 package edu.pacific.comp55.starter;
-/*Designing the rooms
- * partygoers[]
- * item[5] loosestuff
- * convos[] interactables
- * GImage background
- * Fact[] clues
- * Goal indoors;
- * convo ritual
- * room[] adjacent rooms
- */
+
 public class Rooms {
-	partygoers[] partygoers;
+/*	partygoers[] partygoers;
     item[] loosestuff;
     convos[] interactables;
     GImage background;
     Fact[] clues;
     Goal indoors;
     convo ritual;
-    room[] adjacent rooms;	
+    room[] adjacent rooms;	*/
 }

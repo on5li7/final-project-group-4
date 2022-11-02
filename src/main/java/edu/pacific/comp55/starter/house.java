5665ac42e1bf5b667ff71fa47d63cdaec5a14612
Sophie Tran <s_tran39@u.pacific.edu>
2022-11-02 15:08:21 -0700

    created class house, set variables, and created setters/getters

--- src/main/java/edu/pacific/comp55/starter/house.java
+++ src/main/java/edu/pacific/comp55/starter/house.java
@@ -1,5 +1,32 @@
 package edu.pacific.comp55.starter;
 
 public class house {
+	//sets the time of the house
+		private int time;
+		
+		//checks when it's night or day 
+		private Boolean isDark;
+		
+		//the number of dead people
+		private int deadpeople;
+		
+		public int getTime() {
+			return time;
+		}
+		public void setTime(int time) {
+			this.time = time;
+		}
+		public Boolean getIsDark() {
+			return isDark;
+		}
+		public void setIsDark(Boolean isDark) {
+			this.isDark = isDark;
+		}
+		public int getDeadpeople() {
+			return deadpeople;
+		}
+		public void setDeadpeople(int deadpeople) {
+			this.deadpeople = deadpeople;
+		}
 
 }

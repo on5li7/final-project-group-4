f6ed7340ce898179f688530fe9e75ea5dd5e0a93
Clint <c_penafiel@u.pacific.edu>
2022-11-02 14:18:28 -0700

    Starting on Rooms to be implemented in the game.

---
+++ src/main/java/edu/pacific/comp55/starter/Rooms.java
@@ -0,0 +1,16 @@
+package edu.pacific.comp55.starter;
+/*Designing the rooms
+ * partygoers[]
+ * item[5] loosestuff
+ * convos[] interactables
+ * GImage background
+ * Fact[] clues
+ * Goal indoors;
+ * convo ritual
+ * room[] adjacent rooms
+ * 
+ */
+public class Rooms {
+	
+	
+}

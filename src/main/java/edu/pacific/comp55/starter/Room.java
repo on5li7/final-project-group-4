7a26fca838a39bc6e70c89207913d6b2badd26f2
Clint <c_penafiel@u.pacific.edu>
2022-11-02 14:51:06 -0700

    Starting fresh

--- src/main/java/edu/pacific/comp55/starter/Room.java
+++ src/main/java/edu/pacific/comp55/starter/Room.java
@@ -1,12 +1,11 @@
 package edu.pacific.comp55.starter;
 
-public class Rooms {
-/*	partygoers[] partygoers;
-    item[] loosestuff;
-    convos[] interactables;
-    GImage background;
-    Fact[] clues;
-    Goal indoors;
-    convo ritual;
-    room[] adjacent rooms;	*/
+import acm.graphics.GImage;
+
+public class Room {
+//Instance variables
+
 }
+
+
+

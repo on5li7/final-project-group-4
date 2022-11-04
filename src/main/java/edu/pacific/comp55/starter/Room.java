71c951a8d5bd67b67b032cd99f2e932e7533893d
Clint <c_penafiel@u.pacific.edu>
2022-11-04 14:35:24 -0700

    Working on the Room Class, initialized ArrayList for items to add

--- src/main/java/edu/pacific/comp55/starter/Room.java
+++ src/main/java/edu/pacific/comp55/starter/Room.java
@@ -1,9 +1,15 @@
 package edu.pacific.comp55.starter;
 import java.util.*;
 import acm.graphics.GImage;
+import java.util.ArrayList;
 
 public class Room {
 //Instance variables
+	//Rooms include items, NPCs, and exits
+	private String roomName; //RoomName
+	private ArrayList<item> items; //initialized ArrayList for items to add
+
+
 HashSet<Room> adjacentRooms;
 //The bus stops are 1-5. If the room is not a bus stop, this is 0.
 //Apothecary is stop 1. Hallway is stop 2. Armory is stop 3. Kitchen is stop 4. Outdoors bottem left is stop 5.

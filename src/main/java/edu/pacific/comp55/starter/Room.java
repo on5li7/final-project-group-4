a6df7b5d1f74e1c8cf93cfde5665a994a94b42ec
Sophie Tran <s_tran39@u.pacific.edu>
2022-11-09 14:50:45 -0800

    made GImage variable for the background

--- src/main/java/edu/pacific/comp55/starter/Room.java
+++ src/main/java/edu/pacific/comp55/starter/Room.java
@@ -13,6 +13,8 @@ ArrayList<Convos> Interactables;
 
 Convos Ritual;
 
+GImage background;
+
 HashSet<Room> adjacentRooms;
 //The bus stops are 1-5. If the room is not a bus stop, this is 0.
 //Apothecary is stop 1. Hallway is stop 2. Armory is stop 3. Kitchen is stop 4. Outdoors bottem left is stop 5.

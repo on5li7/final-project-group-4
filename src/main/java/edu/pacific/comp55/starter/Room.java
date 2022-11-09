fc20a4ce55cce08937f0cb0d4dc6f153547874a6
Clint <c_penafiel@u.pacific.edu>
2022-11-09 14:37:46 -0800

    Changed the ArrayList to be placed in House instead of Rooms. Created variables for the rooms so that we can try to assign items to them.

--- src/main/java/edu/pacific/comp55/starter/Room.java
+++ src/main/java/edu/pacific/comp55/starter/Room.java
@@ -4,25 +4,29 @@ import acm.graphics.GImage;
 import java.util.ArrayList;
 
 public class Room {
-public static void main(String[] args) {
-	ArrayList<String> Rooms = new ArrayList<String>();
-	Rooms.add("Dining Hall");
-	Rooms.add("Apothecary");
-	Rooms.add("Kitchen");
-	Rooms.add("Wine Cellar");
-	Rooms.add("GreenHouse");
-	Rooms.add("The Study");
-	Rooms.add("Armory");
-	Rooms.add("Workshop");
-	Rooms.add("Dungeon");
-	Rooms.add("Outdoors");
-	Rooms.add("The Cliff");	
-	System.out.println(Rooms);
-}
-
-item[] looseStuff;
-
-Boolean inDoors;
+	public Room DiningHall;
+	public Room Apothecary;
+	public Room Kitchen;
+	public Room WineCellar;
+	public Room GreenHouse;
+	public Room TheStudy;
+	public Room Armory;
+	public Room Workshop;
+	public Room TheCliff;
+	public Room Outdoors_1;
+	public Room Outdoors_2;
+	public Room Outdoors_3;
+	public Room Outdoors_4;
+	public Room Bedroom_1;
+	public Room Bedroom_2;
+	public Room Bedroom_3;
+	public Room Bedroom_4;
+	public Room Bedroom_5;
+	public Room Bedroom_6;
+	public Room Bedroom_7;
+	public Room Bedroom_8;
+	public Room Bedroom_9;
+	public Room Bedroom_10;
 
 HashSet<Room> adjacentRooms;
 //The bus stops are 1-5. If the room is not a bus stop, this is 0.
--- src/main/java/edu/pacific/comp55/starter/house.java
+++ src/main/java/edu/pacific/comp55/starter/house.java
@@ -1,6 +1,32 @@
 package edu.pacific.comp55.starter;
 
+import java.util.ArrayList;
+
 public class house {
+	public static Room DiningHall;
+	public static Room Apothecary;
+	public static Room Kitchen;
+	public static Room WineCellar;
+	public static Room GreenHouse;
+	public static Room TheStudy;
+	public static Room Armory;
+	public static Room Workshop;
+	public static Room TheCliff;
+	public static Room Dungeon;
+	public static Room Outdoors_1;
+	public static Room Outdoors_2;
+	public static Room Outdoors_3;
+	public static Room Outdoors_4;
+	public static Room Bedroom_1;
+	public static Room Bedroom_2;
+	public static Room Bedroom_3;
+	public static Room Bedroom_4;
+	public static Room Bedroom_5;
+	public static Room Bedroom_6;
+	public static Room Bedroom_7;
+	public static Room Bedroom_8;
+	public static Room Bedroom_9;
+	public static Room Bedroom_10;
 	//sets the time of the house
 		private int time;
 		
@@ -31,5 +57,32 @@ public class house {
 		public void setDeadpeople(int deadpeople) {
 			this.deadpeople = deadpeople;
 		}
-
+		public static void main(String[] args) {
+			ArrayList<String> House = new ArrayList<String>();
+			DiningHall = new Room();
+			Apothecary = new Room();
+			Kitchen = new Room();
+			WineCellar = new Room();
+			GreenHouse = new Room();
+			TheStudy = new Room();
+			Armory = new Room();
+			Workshop = new Room();
+			Dungeon = new Room();
+			Outdoors_1 = new Room();
+			Outdoors_2 = new Room();
+			Outdoors_3 = new Room();
+			Outdoors_4 = new Room();
+		    Bedroom_1 = new Room();
+			Bedroom_2 = new Room();
+			Bedroom_3 = new Room();
+			Bedroom_4 = new Room();
+			Bedroom_5 = new Room();
+			Bedroom_6 = new Room();
+			Bedroom_7 = new Room();
+			Bedroom_8 = new Room();
+			Bedroom_9 = new Room();
+			Bedroom_10 = new Room();
+			TheCliff = new Room();	
+			System.out.println(House);
+		}
 }

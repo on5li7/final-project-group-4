4a4eec0c71662eb21c5178562a8cb6e864228c5d
Clint <c_penafiel@u.pacific.edu>
2022-11-09 14:38:23 -0800

    Deleted the Arraylist from Rooms, it is now in house.java

--- src/main/java/edu/pacific/comp55/starter/Room.java
+++ src/main/java/edu/pacific/comp55/starter/Room.java
@@ -4,29 +4,6 @@ import acm.graphics.GImage;
 import java.util.ArrayList;
 
 public class Room {
-	public Room DiningHall;
-	public Room Apothecary;
-	public Room Kitchen;
-	public Room WineCellar;
-	public Room GreenHouse;
-	public Room TheStudy;
-	public Room Armory;
-	public Room Workshop;
-	public Room TheCliff;
-	public Room Outdoors_1;
-	public Room Outdoors_2;
-	public Room Outdoors_3;
-	public Room Outdoors_4;
-	public Room Bedroom_1;
-	public Room Bedroom_2;
-	public Room Bedroom_3;
-	public Room Bedroom_4;
-	public Room Bedroom_5;
-	public Room Bedroom_6;
-	public Room Bedroom_7;
-	public Room Bedroom_8;
-	public Room Bedroom_9;
-	public Room Bedroom_10;
 
 HashSet<Room> adjacentRooms;
 //The bus stops are 1-5. If the room is not a bus stop, this is 0.

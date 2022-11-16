fa3d715d1bc84ac65510ed7550d6266cef315baf
Pro <Pro@10.0.0.64>
2022-11-15 19:53:01 -0800

    Moved some things around. Changed Goals to Goal and made it an enumerator.

--- src/main/java/edu/pacific/comp55/starter/GoalSets.java
+++ src/main/java/edu/pacific/comp55/starter/GoalSets.java
@@ -2,14 +2,20 @@ package edu.pacific.comp55.starter;
 import java.util.*; 
 
 public class GoalSets {
-private HashMap<String, HashSet<Goals>> GoalSets = new HashMap<String, HashSet<Goals>>();
 
-public HashMap<String, HashSet<Goals>> getGoalSets() {
-	return GoalSets;
-}
-
-public void setGoalSets(HashMap<String, HashSet<Goals>> goalSets) {
-	GoalSets = goalSets;
-}
 	
+public static ArrayList<Goal> innocuous;
+public static ArrayList<Goal> sociable;
+public static ArrayList<Goal> drinking;
+public static ArrayList<Goal> poisonous;
+public static ArrayList<Goal> gathering;
+public static ArrayList<Goal> murderous;
+public static ArrayList<Goal> rituals;
+
+public void generateSets() {
+innocuous.add(Goal.EATING);
+innocuous.add(Goal.DRINKING_CELLAR);
+innocuous.add(Goal.GOSSIPING);
+innocuous.add(null);
+}
 }

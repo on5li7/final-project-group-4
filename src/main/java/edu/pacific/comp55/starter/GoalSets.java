8a6757189b7fc6591aa6e9461498f0d5eb39a415
Pro <Pro@10.15.186.194>
2022-11-18 15:18:09 -0800

    Added goal enumerater as well as goalsets

--- src/main/java/edu/pacific/comp55/starter/GoalSets.java
+++ src/main/java/edu/pacific/comp55/starter/GoalSets.java
@@ -14,8 +14,34 @@ public static ArrayList<Goal> rituals;
 
 public void generateSets() {
 innocuous.add(Goal.EATING);
-innocuous.add(Goal.DRINKING_CELLAR);
+innocuous.add(Goal.DRINKING_HALL);
 innocuous.add(Goal.GOSSIPING);
-innocuous.add(null);
+innocuous.add(Goal.RANSACK);
+innocuous.add(Goal.OOHSHINY);
+
+sociable.add(Goal.GOSSIPING);
+sociable.add(Goal.GOSSIPING);
+sociable.add(Goal.GOSSIPING);
+sociable.add(Goal.GOSSIPING);
+
+drinking.add(Goal.DRINKING_CELLAR);
+drinking.add(Goal.DRINKING_HALL);
+drinking.add(Goal.DRINKING_KITCHEN);
+drinking.add(Goal.DRINKING_CELLAR);
+drinking.add(Goal.DRINKING_HALL);
+drinking.add(Goal.DRINKING_KITCHEN);
+drinking.add(Goal.DRINKING_CELLAR);
+drinking.add(Goal.DRINKING_HALL);
+drinking.add(Goal.DRINKING_KITCHEN);
+
+gathering.add(Goal.OOHSHINY);
+gathering.add(Goal.OOHSHINY);
+gathering.add(Goal.RANSACK);
+gathering.add(Goal.RANSACK);
+gathering.add(Goal.RANSACK);
+gathering.add(Goal.RANSACK);
+gathering.add(Goal.GET_KNIFE);
+gathering.add(Goal.GET_RIFLE);
+gathering.add(Goal.GET_SWORD);
 }
 }
